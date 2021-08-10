f41c5c9610c94ba9a4b17730e7f2e7be83854145
jonasrgoes <jonasrgoes@gmail.com>
2021-08-10 10:18:20 -0300

    Revised, updated and tested source code

--- lib/src/utils/values/string_constants.dart
+++ lib/src/utils/values/string_constants.dart
@@ -1,12 +1,28 @@
 class StringConstants {
   static const emailValidateMessage = "Enter a valid email";
   static const financeValueValidateMessage = "the value is not a valid number";
-  static const passwordValidateMessage =
-      "Password must be at least 6 characters";
-  static const displayNameValidateMessage =
-      "Display name must be at least 6 characters";
+  static const passwordValidateMessage = "Password must be at least 6 characters";
+  static const displayNameValidateMessage = "Display name must be at least 6 characters";
   static const emailOrPasswordIncorrect = "Email and/or Password are incorrect";
   static const fillUpFormCorrectly = "You must fill up the form correctly";
-  static const couldNotRegisterUser =
-      "Could not register user. Maybe e-mail is already been taken";
+  static const couldNotRegisterUser = "Could not register user. Maybe e-mail is already been taken";
+  static const login = "Login";
+  static const displayName = "Display Name ...";
+  static const email = "Email ...";
+  static const password = "Password ...";
+  static const noExpenses = "No expenses to show";
+  static const confirm = "Confirm";
+  static const about = "About the App";
+  static const profile = "My Profile";
+  static const settings = "Settings";
+  static const logout = "Logout";
+  static const currentExpense = "Current Expense";
+  static const limit = "Available Limit";
+  static const mostRecentExpense = "Most recent expense worth";
+  static const noRecentExpenses = "No recent expenses";
+  static const monthlyBudget = "Monthly Budget";
+  static const requireMonthlyBudget = "You must set the monthly budget first";
+  static const setMonthlyBudget = "Set monthly Budget";
+  static const insertExpense = "Insert an Expense";
+  static const tellAFriend = "Tell a Friend";
 }
